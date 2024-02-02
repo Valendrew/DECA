fix: the computation of flame vertices is without any pose (neither jaw nor rotation)

decalib/deca.py
@@ -136,7 +136,7 @@ class DECA(nn.Module):
         return vis68
 
     # @torch.no_grad()
-    def encode(self, images, use_detail=True):
+    def encode(self, images, use_detail=True, remove_jaw_pose=False):
         if use_detail:
             # use_detail is for training detail model, need to set coarse model as eval mode
             with torch.no_grad():
@@ -153,7 +153,11 @@ class DECA(nn.Module):
             euler_jaw_pose = posecode[:,3:].clone() # x for yaw (open mouth), y for pitch (left ang right), z for roll
             posecode[:,3:] = batch_euler2axis(euler_jaw_pose)
             codedict['pose'] = posecode
-            codedict['euler_jaw_pose'] = euler_jaw_pose  
+            codedict['euler_jaw_pose'] = euler_jaw_pose 
+        if remove_jaw_pose:
+            posecode = codedict['pose']
+            posecode[:,3:] = torch.zeros_like(posecode[:,3:])
+            codedict['pose'] = posecode
         return codedict
 
     # @torch.no_grad()
@@ -163,7 +167,7 @@ class DECA(nn.Module):
         batch_size = images.shape[0]
         
         ## decode
-        verts, landmarks2d, landmarks3d, verts_wo_rot = self.flame(shape_params=codedict['shape'], expression_params=codedict['exp'], pose_params=codedict['pose'])
+        verts, landmarks2d, landmarks3d, verts_wo_pose = self.flame(shape_params=codedict['shape'], expression_params=codedict['exp'], pose_params=codedict['pose'])
         if self.cfg.model.use_tex:
             albedo = self.flametex(codedict['tex'])
         else:
@@ -176,7 +180,7 @@ class DECA(nn.Module):
         trans_verts = util.batch_orth_proj(verts, codedict['cam']); trans_verts[:,:,1:] = -trans_verts[:,:,1:]
         opdict = {
             'verts': verts,
-            'verts_wo_rot': verts_wo_rot,
+            'verts_wo_pose': verts_wo_pose,
             'trans_verts': trans_verts,
             'landmarks2d': landmarks2d,
             'landmarks3d': landmarks3d,

decalib/models/FLAME.py
@@ -197,8 +197,8 @@ class FLAME(nn.Module):
                           self.lbs_weights, dtype=self.dtype)
         
         # Compute the vertices without rotation
-        full_pose_no_rot = torch.cat([torch.zeros_like(pose_params[:, :3]), self.neck_pose.expand(batch_size, -1), pose_params[:, 3:], eye_pose_params], dim=1)
-        vertices_wo_rot, _ = lbs(betas, full_pose_no_rot, template_vertices,
+        full_pose_no_rot = torch.cat([torch.zeros_like(pose_params[:, :3]), self.neck_pose.expand(batch_size, -1), torch.zeros_like(pose_params[:, 3:]), eye_pose_params], dim=1)
+        vertices_wo_pose, _ = lbs(betas, full_pose_no_rot, template_vertices,
                                     self.shapedirs, self.posedirs,
                                     self.J_regressor, self.parents,
                                     self.lbs_weights, dtype=self.dtype)
@@ -220,7 +220,7 @@ class FLAME(nn.Module):
         landmarks3d = vertices2landmarks(vertices, self.faces_tensor,
                                        self.full_lmk_faces_idx.repeat(bz, 1),
                                        self.full_lmk_bary_coords.repeat(bz, 1, 1))
-        return vertices, landmarks2d, landmarks3d, vertices_wo_rot
+        return vertices, landmarks2d, landmarks3d, vertices_wo_pose
 
 class FLAMETex(nn.Module):
     """